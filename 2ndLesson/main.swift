1e6dbe2b1c7ccedfbdfd3c8f5b414f9d96b7d6cf
sharshenovra <sharshenovr@icloud.com>
2022-09-12 19:04:05 +0600

    Initial Commit

---
+++ 2ndLesson/main.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+//func printString(string: String) {
+//    print(string)
+//}
+//
+//printString(string: "ABC")
+//printString(string: "ugugu")
+
+
+func solveSquare(num: Int) {
+    print("Квадрат \(num) равен \(num * num)")
+}
+
+solveSquare(num: 6)
