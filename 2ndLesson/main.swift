a5b90b1b0ee2e5a09b3a353b923a12f360e601db
sharshenovra <sharshenovr@icloud.com>
2022-09-12 19:14:42 +0600

    No comments

--- 2ndLesson/main.swift
+++ 2ndLesson/main.swift
@@ -1,13 +1,5 @@
 import Foundation
 
-//func printString(string: String) {
-//    print(string)
-//}
-//
-//printString(string: "ABC")
-//printString(string: "ugugu")
-
-
 func solveSquare(num: Int) {
     print("Квадрат \(num) равен \(num * num)")
 }
